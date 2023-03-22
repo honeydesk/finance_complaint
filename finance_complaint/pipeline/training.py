cb57d3cab52bf0650a4f3b567922d2a0a65f1514
honeydesk <honeywarid@gmail.com>
2023-03-23 01:00:05 +0530

    model evaluation completed

--- finance_complaint/pipeline/training.py
+++ finance_complaint/pipeline/training.py
@@ -1,11 +1,12 @@
 from finance_complaint.exception import FinanceException
 from finance_complaint.logger import logging as  logger
-from finance_complaint.entity.config_entity import (DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig)
-from finance_complaint.entity.artifact_entity import (DataIngestionArtifact,DataValidationArtifact,DataTransformationArtifact,ModelTrainerArtifact)
+from finance_complaint.entity.config_entity import (DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig,ModelEvaluationConfig)
+from finance_complaint.entity.artifact_entity import (DataIngestionArtifact,DataValidationArtifact,DataTransformationArtifact,ModelTrainerArtifact,ModelEvaluationArtifact)
 from finance_complaint.component.data_ingestion import DataIngestion
 from finance_complaint.component.data_validation import DataValidation  
 from finance_complaint.component.data_transformation import DataTransformation 
 from finance_complaint.component.model_trainer import ModelTrainer  
+from finance_complaint.component.model_evaluation import ModelEvaluation
 import os,sys
 
 
@@ -58,6 +59,17 @@ class TrainingPipeline:
         except Exception as e:
             raise FinanceException(e, sys)
 
+    def start_model_evaluation(self, data_validation_artifact, model_trainer_artifact) -> ModelEvaluationArtifact:
+        try:
+            model_eval_config = ModelEvaluationConfig(training_pipeline_config=self.training_pipeline_config)
+            model_eval = ModelEvaluation(data_validation_artifact=data_validation_artifact,
+                                        model_trainer_artifact=model_trainer_artifact,
+                                        model_eval_config=model_eval_config
+                                        )
+            return model_eval.initiate_model_evaluation()
+        except Exception as e:
+            raise FinanceException(e, sys)
+
     
     def start(self):
         try:
@@ -65,6 +77,12 @@ class TrainingPipeline:
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            model_eval_artifact = self.start_model_evaluation(data_validation_artifact=data_validation_artifact,
+                                                              model_trainer_artifact=model_trainer_artifact
+                                                              )
+
+            if model_eval_artifact.model_accepted:
+                pass
             
         except Exception as e:
             raise FinanceException(e, sys)
